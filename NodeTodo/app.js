3fa9965849fb6585656a22672f7be351077682b3
Emin Vilgenoğlu <evilgenoglu@akdenizkimya.com.tr>
2019-05-26 15:05:44 +0300

    app.js port changed to 5000

--- NodeTodo/app.js
+++ NodeTodo/app.js
@@ -6,7 +6,7 @@ var config = require('./config');
 var setupController = require('./controllers/setupController');
 var apiController = require('./controllers/apiController');
 
-var port = process.env.PORT || 30000;
+var port = process.env.PORT || 5000;
 app.use('/', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs'); 
 
